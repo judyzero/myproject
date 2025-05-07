2d0eb46e60acc9392f7a07b3800334e0445e6355
judyzero <qozmcll@kookmin.ac.kr>
2025-05-07 14:29:17 +0900

    HTTP 요청 응답 메시지 실습

---
+++ myapp/views.py
@@ -0,0 +1,23 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+@api_view(['GET'])
+def hello_api(request):
+    return Response({"message": "안녕하세요, DRF API입니다!"})
+
+@api_view(['POST'])
+def receive_post(request):
+    name = request.data.get('name')
+    message = request.data.get('message')
+
+    if not name or not message:
+        return Response(
+            {"error": "이름과 메시지를 모두 입력해야 합니다."}, 
+            status=status.HTTP_400_BAD_REQUEST
+            )
+    
+    return Response(
+        {"message": f"{name}님이 보낸 메시지: {message}"},
+        status=status.HTTP_201_CREATED
+        )
--- myproject/settings.py
+++ myproject/settings.py
@@ -34,6 +34,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'accounts',
     'request_test',
+    'myapp',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
