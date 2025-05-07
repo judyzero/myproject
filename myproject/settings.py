69492d25cc1ce15b7555c1af62626f1021b99069
judyzero <qozmcll@kookmin.ac.kr>
2025-05-07 14:03:40 +0900

    로그인/로그아웃 기능 실습

--- myproject/settings.py
+++ myproject/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'request_test',
     'django.contrib.admin',
     'django.contrib.auth',
--- myproject/urls.py
+++ myproject/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('request_test.urls')),
+    path('', include('accounts.urls')),
 ]
 
 if settings.DEBUG:
